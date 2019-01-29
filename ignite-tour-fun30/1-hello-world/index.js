3fea3285b63d0ebda69df6599703b4709db07402
Neil Peterson <neilpeterson@outlook.com>
2019-01-29 20:21:34 +0100

    Test Commit

--- ignite-tour-fun30/1-hello-world/index.js
+++ ignite-tour-fun30/1-hello-world/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    message = 'Hello World';
+    message = 'Hello World, this is so cool.';
     figlet(message, (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
