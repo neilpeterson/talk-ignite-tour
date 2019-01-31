2927daad6774980de38a611ab774072a1237ef71
Neil Peterson <neilpeterson@outlook.com>
2019-01-31 12:32:39 +0100

    Updated string

--- ignite-tour-fun30/1-hello-world/index.js
+++ ignite-tour-fun30/1-hello-world/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    message = 'Hello World.';
+    message = 'Hello Milan.';
     figlet(message, (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
